fd076caa6039e297f37283adc2a9448d7679bac9
Theo Bogdan <theo.m.bogdan@gmail.com>
2026-02-02 18:52:03 +0200

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,6 @@
+﻿const { test, expect } = require('@playwright/test');
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle(/Example/);
+});
